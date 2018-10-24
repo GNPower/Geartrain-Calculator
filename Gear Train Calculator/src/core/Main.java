82bc179fd1a05b8bee9ca3218f13c83fd9bb4027
Maanav Dalal <maanavdalal@gmail.com>
2018-10-23 20:52:11 -0400

    This is another test.

--- Gear Train Calculator/src/core/Main.java
+++ Gear Train Calculator/src/core/Main.java
@@ -4,7 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Graham. This is a test.")
+		System.out.println("Hello Graham. This is a test.");
+		int minTeeth = 12;
+		double minModule = 1.0;
+		int teamNum = 0;
+		double speedFactor;
+		if (teamNum < 5)
+			speedFactor = 42.33;
+		else if (teamNum < 11)
+			speedFactor = 10.25;
+		else if (teamNum < 35)
+			speedFactor = 3.55;
+		else if (teamNum < 100)
+			speedFactor = 1.33;
+		else
+			speedFactor = 0.84;
+		int inputSpeed = speedFactor*teamNum;
+
+
 	}
 
 }
