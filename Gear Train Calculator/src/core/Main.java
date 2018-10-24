b2a449f3fc485f38d9c2320fd105e83549bb494a
Graham <powergraham907@gmail.com>
2018-10-23 20:46:32 -0400

    file writer for constraints loaded

--- Gear Train Calculator/src/core/Main.java
+++ Gear Train Calculator/src/core/Main.java
@@ -1,10 +1,13 @@
 package core;
 
+import core.files.Constants;
+import core.files.Reader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Reader.readConstraints("./res/constraints.txt");
+		Constants.getConstants().printConstants();
 	}
 
 }
